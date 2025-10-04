76398bc0ceb9a93304fa0ba89e710fd409155b54
Abd El Rahman Ali <abdo93431@gmail.com>
2025-10-04 20:04:28 +0300

    fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,8 +9,29 @@ const PORT=process.env.PORT
 
 const app=express();
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+  next();
+});
+
+
 app.use(helmet())
 app.use(express.json())
+
+
+app.use((req, res, next) => {
+  console.log(`>>> ${req.method} ${req.url}`);
+  next();
+});
+
 const allowedOrigins = [
   "http://localhost:3000",
   "https://book-assignment-beta.vercel.app",
